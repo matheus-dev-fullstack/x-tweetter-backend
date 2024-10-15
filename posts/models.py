15516f432ed574c98ad1f4db69de81cb4dc678bd
Matheus Oliveira <matheuseu41@gmail.com>
2024-10-15 20:49:55 -0300

    Definindo models para Usuários e Posts e gerando requirements.txt

---
+++ posts/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from usuarios.models import Usuario
+
+class Post(models.Model):
+    content = models.CharField(max_length=300)
+    author = models.ForeignKey(Usuario,  on_delete=models.CASCADE)
+    released = models.DateTimeField(auto_now_add=True)
+    
+class Imagem(models.Model):
+    image = models.ImageField(upload_to='imagem_post')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='imagens')
+    
+    
+class Like(models.Model):
+    user = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
+
+    def __str__(self):
+        return f'{self.user.name} liked {self.post.id}'
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    content = models.TextField(max_length=280)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.author.name} commented on {self.post.id}'
---
+++ usuarios/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+class Usuario(models.Model):
+    name = models.CharField(max_length=100)
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    isVerified = models.BooleanField(default=False)
+    about = models.TextField(max_length=200, blank=True)
+    
+    def save(self, *args, **kwargs):
+        if not self.password.startswith('pbkdf2_'):  # Verifica se já é um hash
+            self.password = make_password(self.password)  # Gera o hash da senha
+        super().save(*args, **kwargs)
+        
+        def __str__(self):
+            return self.nome
