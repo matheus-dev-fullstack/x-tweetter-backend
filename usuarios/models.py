4ffb7136da8098dad0c4248706d51bb8a52481ec
matheus-dev-fullstack <matheuseu41@gmail.com>
2024-11-01 21:40:33 -0300

    Ajustando foto de perfil no model

--- usuarios/models.py
+++ usuarios/models.py
@@ -10,7 +10,7 @@ class Usuario(AbstractUser):
     password = models.CharField(max_length=100)
     isVerified = models.BooleanField(default=False)
     about = models.TextField(max_length=200, blank=True)
-    # perfilPhoto = models.ImageField(upload_to='perfilPhoto', blank=True, default="perfilPhoto/perfil-default.jpg", null=True)
+    perfilPhoto = models.ImageField(upload_to='perfilPhoto', blank=True, default="perfilPhoto/perfil-default.jpg", null=True)
     
     def save(self, *args, **kwargs):
         if not self.password.startswith('pbkdf2_'):  # Verifica se já é um hash
